cc4740e844bed779f2642d1a5a6ff3c8a4a47db5
Nathan <57403690+iNatee@users.noreply.github.com>
2019-12-16 00:50:25 +0000

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,25 @@
+class Loops:
+
+  # Example file for working with loops
+  #
+
+  def main(self):
+    x = 0
+
+    # define a while loop
+
+
+    # define a for loop
+
+
+    # use a for loop over a collection
+
+
+    # use the break and continue statements
+
+
+    #using the enumerate() function to get index
+
+
+  if __name__ == "__main__":
+    main()
