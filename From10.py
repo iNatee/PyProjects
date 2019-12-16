c2563afb685a74d1bee4ce0ba9aed0195e2b2ff1
iNatee <nathanmurray1999@hotmail.co.uk>
2019-12-16 03:13:37 +0000

    From PyCharm for Mac on Mon 16 Dec 2:06am

---
+++ From10.py
@@ -0,0 +1,24 @@
+class UserInput:
+
+    def operateTo10(userOp, userNum):
+
+        if userOp == "+":
+            return 10 + userNum
+
+        elif userOp == "-":
+
+            if userNum < 10:
+                return 10 - userNum
+
+            else:
+                return userNum - 10
+
+    goAgain = "Yes"
+    while goAgain == "Yes":
+        userOp = raw_input("Which operation to do to 10?\n")
+        userIn = raw_input("Which number do you want to " + str(userOp) + " 10?\n")
+        userNum = int(userIn)
+        print("10 " + str(userOp) + " " + str(userNum) + " = " + str(operateTo10(userOp, userNum)))
+        goAgain = raw_input("Do you want to do the from 10 game?\n")
+
+    print("Goodbye :)")
